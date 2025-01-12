8770818dba7b8decf81f2e080ec679fadca57984
AfrozKhan <pathanafrozkhan786@gmail.com>
2025-01-12 19:36:42 +0530

    program for second largest number

---
+++ src/java8/Lambda/sorting.java
@@ -0,0 +1,34 @@
+package src.java8.Lambda;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class sorting {
+    public static void main(String[] args) {
+        List<Integer> sort = Arrays.asList(12, 24, 26, 15, 84, 36, 88, 75);
+        List<Integer> secondLargeUnique = Arrays.asList(12, 3, 11,4,4,3, 2, 5);
+        sort.stream().sorted(Comparator.reverseOrder()).forEach(System.out::println);
+
+        Optional<Integer> secondlargestNumber = findSecondlargestNumber(secondLargeUnique);
+
+
+
+    }
+
+    public static Optional<Integer>findSecondlargestNumber(List<Integer> second)
+    {
+        Map<Integer, Long> frequencyMap = second.stream()
+                                                .collect(Collectors.groupingBy(n -> n, Collectors.counting()));
+
+        // Filter unique numbers and sort in descending order
+         List<Integer> uniqueNumbers = frequencyMap.entrySet().stream()
+                                                   .filter(entry -> entry.getValue() == 1)
+                                                    .map(Map.Entry::getKey)
+                                                    .sorted(Comparator.reverseOrder())
+                                                    .collect(Collectors.toList());
+        System.out.println(uniqueNumbers);
+         // Find the second largest unique number
+         return uniqueNumbers.size() > 1 ? Optional.of(uniqueNumbers.get(1)) : Optional.empty(); }
+    }
+
