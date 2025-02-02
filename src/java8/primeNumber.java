9e06329967afb70c9f34a5985ba33be757425bd4
AfrozKhan <pathanafrozkhan786@gmail.com>
2025-02-02 11:14:08 +0530

    updated

---
+++ src/java8/primeNumber.java
@@ -0,0 +1,26 @@
+package src.java8;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class primeNumber {
+    public static void main(String[] args) {
+        System.out.println(isPrime(12));
+
+    }
+
+    public static boolean isPrime(int Num) {
+        boolean flag = true;
+        if (Num == 0 || Num == 1) {
+            System.out.println("please enter number more than 1");
+        } else {
+            for (int i = 2; i <= Num / 2; ++i) {
+                if (Num % i == 0) {
+                    flag = false;
+                    break;
+                }
+            }
+        }
+        return flag;
+    }
+}
